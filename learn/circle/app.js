83b5d0422284981fbc5bff25d0ec91a495083aa5
jungang <jungang@vip.sina.com>
2019-01-22 17:35:56 +0800

    bump package version

--- learn/circle/app.js
+++ learn/circle/app.js
@@ -25,5 +25,15 @@ $(function () {
 
 	init();
 
+	d3.select("body").style("background-color", "black");  // 直接选择 ->并设置属性
+	d3.selectAll("p").style("color", function() { // 选择 -> 动态设置属性
+		return "hsl(" + Math.random() * 360 + ",100%,50%)";
+	});
+	d3.selectAll("p") // 选择 -> 绑定数据 -> 动态设置属性
+		.data([4, 8, 15, 16, 23, 42])
+		.style("font-size", function(d) { return d + "px"; });
+
+
+
 
 })
