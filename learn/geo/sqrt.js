00b7e35ff3c2514ee338b476be9bdab15737c85e
jungang <jungang@vip.sina.com>
2019-01-31 11:36:56 +0800

    bump package version

---
+++ learn/geo/sqrt.js
@@ -0,0 +1,23 @@
+"use strict";
+/**
+ * Created by jg on 2019.1.30.0030.
+ */
+
+
+
+let a = 12
+let b = 8
+
+console.log(Math.sqrt(2));
+
+let sqrt2 = Math.sqrt(2)
+
+    sqrt2 = 1/sqrt2
+
+console.log(sqrt2);
+
+console.log(sqrt2*a);
+console.log(sqrt2*b);
+
+
+console.log(Math.sqrt(18));
