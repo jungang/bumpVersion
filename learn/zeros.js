e732b400480e01c43865670773381d95bbf857bd
jungang <jungang@vip.sina.com>
2019-02-28 16:12:42 +0800

    bump package version

---
+++ learn/zeros.js
@@ -0,0 +1,16 @@
+"use strict";
+/**
+ * Created by jg on 2/28/2019.
+ */
+
+
+function zeros (n) {
+
+    console.log(n)
+    var zs = 0;
+    while(n>0){
+        n=Math.floor(n/5);
+        zs+=n
+    }
+    return zs;
+}
