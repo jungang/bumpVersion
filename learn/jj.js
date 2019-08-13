94aea7905259d38cf41c1874699c14e29a3300d9
jungang <jungang@vip.sina.com>
2019-08-13 15:33:41 +0800

    bump package version

--- learn/jj.js
+++ learn/jj.js
@@ -1,3 +1,39 @@
+"use strict";
+/**
+ * Created by jg on 2019-07-22.
+ */
+
+
+class Observer {
+	constructor(data) {
+		// your code
+	}
+
+	$on() {
+		// your code
+	}
+	$emit() {
+		// your code
+	}
+}
+
+const w = new Observer({a: 1});
+w.$on('a', (v) => {
+	console.log('first ', v)
+})
+
+w.$on('a', (v) => {
+	console.log('second ', v)
+})
+
+w.a = 2; // console: first 2  second 2
+
+w.$emit('a', 3); // console: first 3  second 3
+
+w.a === 3; // true
+
+
+//////////////////////////
 // 4. 请说出以下代码打印的结果
 async function async1() {
 	console.log('async1 start');
